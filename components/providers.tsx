122159bf4d1aa3cdd674e9afb58f9b16938c0303
curse <58047023+cursecodes@users.noreply.github.com>
2023-04-05 19:40:03 -0600

    hosting pricing comparison

---
+++ components/providers.tsx
@@ -0,0 +1,138 @@
+import { useEffect, useState } from "react";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
+
+const providers = [
+  {
+    name: "Railway",
+    url: "https://railway.app/pricing",
+    cpu: 10,
+    memory: 10,
+  },
+  {
+    name: "Hop.io",
+    url: "https://hop.io/pricing",
+    cpu: 12.96,
+    memory: 2.1,
+    storage: 0.15,
+  },
+  {
+    name: "Fly.io",
+    url: "https://fly.io/docs/about/pricing/#apps",
+    cpu: 21,
+    memory: 5,
+    storage: 0.15,
+  },
+  {
+    name: "AWS Fargate",
+    url: "https://aws.amazon.com/fargate/pricing/",
+    cpu: 29.56,
+    memory: 3.24,
+    storage: 0.08,
+    notes:
+      "AWS Fargate is meant to be run on-request. This is pricing for a long-running container. (730 hours/1 month)",
+  },
+  {
+    name: "AWS EC2 On-Demand (A1)",
+    url: "https://aws.amazon.com/ec2/pricing/on-demand/",
+    cpu: 9.3,
+    memory: 9.3,
+    storage: 0.08,
+    notes:
+      "CPU is only available in certain configurations. Please check pricing for more details.",
+  },
+  {
+    name: "AWS EC2 On-Demand (T4G)",
+    url: "https://aws.amazon.com/ec2/pricing/on-demand/",
+    memory: 1.53,
+    cpu: 1.53,
+    storage: 0.08,
+    notes:
+      "CPU is only available in certain configurations. Please check pricing for more details.",
+  },
+  {
+    name: "AWS EC2 On-Demand (T3)",
+    url: "https://aws.amazon.com/ec2/pricing/on-demand/",
+    cpu: 3.8,
+    memory: 3.8,
+    storage: 0.08,
+    notes:
+      "CPU is only available in certain configurations. Please check pricing for more details.",
+  },
+];
+
+export const Providers = ({
+  cpus,
+  ram,
+  storage,
+}: {
+  cpus: number;
+  ram: number;
+  storage: number;
+}) => {
+  const [parent] = useAutoAnimate();
+  const [sortedProviders, setSortedProviders] = useState(
+    providers.sort((a, b) => {
+      const aTotal =
+        a.cpu * cpus + a.memory * ram + (a.storage ? a.storage * storage : 0);
+      const bTotal =
+        b.cpu * cpus + b.memory * ram + (b.storage ? b.storage * storage : 0);
+      return aTotal - bTotal;
+    })
+  );
+  useEffect(() => {
+    setSortedProviders(
+      providers.sort(
+        (a, b) =>
+          a.cpu * cpus +
+          a.memory * ram +
+          (a.storage ? a.storage * storage : 0) -
+          (b.cpu * cpus + b.memory * ram) +
+          (b.storage ? b.storage * storage : 0)
+      )
+    );
+  }, [cpus, ram, storage]);
+  return (
+    <div ref={parent}>
+      {sortedProviders.map((provider) => (
+        <div
+          key={provider.name}
+          className="p-4 border shadow-md rounded-md mb-4 max-w-2xl"
+        >
+          <h2 className="text-lg font-semibold mb-2">{provider.name}</h2>
+          <p>CPU: ${(provider.cpu * cpus).toFixed(2)}</p>
+          <p>RAM: ${(provider.memory * ram).toFixed(2)}</p>
+          <p>
+            Storage:{" "}
+            {provider.storage
+              ? `$${(provider.storage * storage).toFixed(2)}`
+              : "Storage not offered"}
+          </p>
+          <div className="border-b border-white my-2" />
+          <p>
+            Additional Notes:
+            {provider.notes ? ` ${provider.notes}` : " None"}
+          </p>
+          <div className="mt-2" />
+          <p>
+            Total:{" "}
+            {provider.storage
+              ? `$${(
+                  provider.cpu * cpus +
+                  provider.memory * ram +
+                  provider.storage * storage
+                ).toFixed(2)}`
+              : `$${(provider.cpu * cpus + provider.memory * ram).toFixed(2)}`}
+          </p>
+          <div className="mt-2" />
+          <a
+            className=" text-blue-500 underline"
+            href={provider.url}
+            target="_blank"
+          >
+            Go to pricing
+          </a>
+        </div>
+      ))}
+    </div>
+  );
+};
---
+++ pages/index.tsx
@@ -0,0 +1,68 @@
+import { Slider } from "@/components/ui/slider";
+import { useState } from "react";
+import { Providers } from "@/components/providers";
+
+export default function Home() {
+  const [cpus, setCpus] = useState<number[]>([4]);
+  const [ram, setRam] = useState<number[]>([8]);
+  const [storage, setStorage] = useState<number[]>([100]);
+  return (
+    <main className="mt-12">
+      <div className="flex justify-center text-center mb-12">
+        <div>
+          <h1 className="text-4xl font-bold mb-2">
+            Hosting pricing comparison
+          </h1>
+          <p>
+            Built by{" "}
+            <a href="https://cursecode.me" className="font-medium">
+              curse
+            </a>
+          </p>
+        </div>
+      </div>
+      <div className="flex justify-center">
+        <div className="w-96">
+          <div className="">
+            <Slider
+              step={1}
+              max={16}
+              min={0.5}
+              value={cpus}
+              onValueChange={setCpus}
+              color="red"
+            />
+            <p className="font-mono mt-1 text-sm">
+              {cpus} CPU{cpus[0] !== 1 ? "s" : undefined}
+            </p>
+          </div>
+          <div className="mt-6">
+            <Slider
+              step={1}
+              max={32}
+              min={0.5}
+              value={ram}
+              onValueChange={setRam}
+            />
+            <p className="font-mono mt-1 text-sm">{ram} GB RAM</p>
+          </div>
+          <div className="mt-6 ">
+            <Slider
+              step={1}
+              max={1024}
+              min={0}
+              value={storage}
+              onValueChange={setStorage}
+            />
+            <p className="font-mono mt-1 text-sm">
+              {storage[0] > 0 ? `${storage} GB Storage` : "No Storage"}
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-center mt-8">
+        <Providers cpus={cpus[0]} ram={ram[0]} storage={storage[0]} />
+      </div>
+    </main>
+  );
+}
